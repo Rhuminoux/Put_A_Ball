d0d4a094d475f382a0f1e3d79720ea4ec7370b83
Romain DRIAT <romain.driat@epitech.eu>
2019-01-04 16:19:09 +0100

    project

---
+++ Put a ball/Assets/Scripts/Hole.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+
+    public enum SIDE
+    {
+        LEFT,
+        RIGHT
+    }
+
+    public SIDE side;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ///could have only use side as an int +1 -1 but enum is cooler u-u
+        if (side == SIDE.RIGHT)
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + (float)0.03, gameObject.transform.position.y, gameObject.transform.position.z);
+        else
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - (float)0.03, gameObject.transform.position.y, gameObject.transform.position.z);
+        if (side == SIDE.LEFT && transform.position.x < -2.3)
+            side = SIDE.RIGHT;
+        else if (side == SIDE.RIGHT && transform.position.x > 1.2) //////////////// moving side depends on the x position of the object
+            side = SIDE.LEFT;
+    }
+}
